fdb3bc0fa89c64f0dab5b06c613a1a1a6b7feee5
Faf4a <87046111+Faf4a@users.noreply.github.com>
2023-11-23 14:40:38 +0100

    test: replit (linux) customized setup

--- src/args/prompt.js
+++ src/args/prompt.js
@@ -6,83 +6,100 @@ import * as installer from "../../src/helpers/cmd.js";
 import path from "path";
 import chalk from "chalk";
 import { fileURLToPath } from "url";
+import os from "os";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 async function promptUser() {
-  return await inquirer.prompt([
+  const questions = [
     {
       type: "input",
       name: "directory",
-      message: "project directory:",
-      default: "./aoijs",
+      message: "Project Directory (press enter to skip):",
+      default: "./",
     },
     {
       type: "input",
       name: "token",
-      message: "discord bot token:",
-      transformer: (input) => {
-        return input.indexOf(".") !== -1
-          ? input.substring(0, input.indexOf(".")) +
-              "*".repeat(input.length - input.indexOf("."))
-          : input;
-      },
+      message: "Discord Bot Token (press enter to skip):",
+      default: "",
     },
     {
       type: "input",
       name: "prefix",
-      message: "discord bot prefix:",
+      message: "Discord Bot Prefix (press enter to skip):",
       default: "!",
     },
     {
       type: "list",
       name: "setup",
-      message: "template",
-      choices: ["default", "default with handler", "sharding"],
-      default: "default",
+      message: "Template you want to use: (press enter to skip)",
+      choices: ["Starter", "Advanced", "Sharding"],
+      default: "Starter",
     },
     {
       type: "confirm",
       name: "install-deps",
-      message: "pre-install packages?",
+      message:
+        "Do you want to install all needed packages (press enter to skip):",
       default: "Yes",
     },
     {
       type: "confirm",
       name: "install-aoimusic",
-      message: "install aoi.music?",
+      message:
+        "Do you want to implement music in your bot (press enter to skip):",
       default: "Yes",
     },
-  ]);
+  ];
+
+  if (os.platform() === "linux") {
+    questions.splice(2, 0, {
+      type: "confirm",
+      name: "isreplit",
+      message: "Are you using replit? (press enter to skip):",
+      default: "No",
+    });
+  }
+
+  return await inquirer.prompt(questions);
 }
 
 async function modifyIndex(answers, directoryPath) {
-  const { prefix, token, setup, "install-aoimusic": useMusic } = answers;
+  const {
+    prefix,
+    token,
+    setup,
+    "install-aoimusic": useMusic,
+    isreplit,
+  } = answers;
 
   let mainFileContent = fs
     .readFileSync(path.join(__dirname, "../template/index.template.js"), "utf8")
-    .replace(/{TOKEN}/g, token)
-    .replace(/{PREFIX}/g, prefix);
+    .replace(/{TOKEN}/g, isreplit === "Yes" ? "" : token)
+    .replace(/{PREFIX}/g, isreplit === "Yes" ? "" : prefix);
 
   let shardingFileContent = fs
     .readFileSync(
       path.join(__dirname, "../template/sharding.template.js"),
       "utf8"
     )
-    .replace(/{TOKEN}/g, token)
-    .replace(/{PREFIX}/g, prefix);
+    .replace(/{TOKEN}/g, isreplit === "Yes" ? "" : token)
+    .replace(/{PREFIX}/g, isreplit === "Yes" ? "" : prefix);
 
   let handlerFileContent = fs
     .readFileSync(
       path.join(__dirname, "../template/handler.template.js"),
       "utf8"
     )
-    .replace(/{TOKEN}/g, token)
-    .replace(/{PREFIX}/g, prefix);
+    .replace(/{TOKEN}/g, isreplit === "Yes" ? "" : token)
+    .replace(/{PREFIX}/g, isreplit === "Yes" ? "" : prefix);
+
   let handlerExampleContent = fs.readFileSync(
     path.join(__dirname, "../template/handler-example.template.js"),
     "utf8"
   );
+
   let aoimusicsetup = fs
     .readFileSync(
       path.join(__dirname, "../template/aoi-music.template.js"),
@@ -92,11 +109,11 @@ async function modifyIndex(answers, directoryPath) {
 
   fs.writeFileSync(
     path.join(directoryPath, "index.js"),
-    setup === "default with handler" ? handlerFileContent : mainFileContent
+    setup === "Advanced" ? handlerFileContent : mainFileContent
   );
 
   if (
-    setup === "default with handler" &&
+    setup === "Advanced" &&
     !fs.existsSync(path.join(directoryPath, "commands"))
   ) {
     fs.mkdirSync(path.join(directoryPath, "commands"), { recursive: true });
@@ -121,7 +138,7 @@ async function modifyIndex(answers, directoryPath) {
     );
   }
 
-  if (answers.setup === "sharding") {
+  if (answers.setup === "Sharding") {
     fs.writeFileSync(
       path.join(directoryPath, "sharding.js"),
       shardingFileContent
@@ -172,12 +189,31 @@ try {
     )
   );
 
+  if (answers.isreplit) {
+    console.log(
+      `\n\r${chalk.bgYellow(
+        " WARN "
+      )} You are using replit, so you need to add your token and prefix in a enviorment variable called ${chalk.gray(
+        "token"
+      )} and ${chalk.gray("prefix")}.`
+    );
+  }
+
+  if (answers.token === "" && !answers.isreplit)
+    console.log(
+      `\n\r${chalk.bgYellow(
+        " WARN "
+      )} You didn't provide a token, so you need to add your token in your ${
+        answers.setup === "Sharding"
+          ? `${chalk.gray("index.js")} & ${chalk.gray("sharding.js")}`
+          : `${chalk.gray("index.js")}`
+      } file.`
+    );
+
   console.log(
-    `\n\rYou can now start your bot using the command \`node .\`\n\rIf you have any issues feel free to join our Discord (${chalk.blue.underline(
+    `\n\rYou can now start your bot using the command \`node .\`\n\rIf you have any issues feel free to join our Discord (${chalk.underline(
       "https://aoi.js.org/invite"
-    )}) or check our documentation (${chalk.blue.underline(
-      "https://aoi.js.org/"
-    )}).`
+    )}) or check our documentation (${chalk.underline("https://aoi.js.org/")}).`
   );
 } catch (error) {
   console.error("Something went wrong, failed to handle:", error);
