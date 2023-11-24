9b54bbbf22812f7490b460096c8f9ef99cc62fc4
Faf4a <87046111+Faf4a@users.noreply.github.com>
2023-11-24 09:30:03 +0100

    improve version cmd

--- src/args/version.js
+++ src/args/version.js
@@ -1,11 +1,59 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
+
+import * as installer from "../../src/helpers/cmd.js";
+import { fileURLToPath } from "url";
 
 import chalk from "chalk";
 import fs from "fs";
 import path from "path";
-import { fileURLToPath } from "url";
+import inquirer from "inquirer";
+import PressToContinuePrompt from "inquirer-press-to-continue";
+
+inquirer.registerPrompt("press-to-continue", PressToContinuePrompt);
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const { version } = JSON.parse(fs.readFileSync(path.join(__dirname, "../../package.json"), "utf8"));
+let latestVersion;
+const { version } = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "../../package.json"), "utf8")
+);
 
 console.log(`${chalk.cyan("v" + version)}`);
+
+const checkpkgoutdated = async (pkg) => {
+  try {
+    const response = await fetch(`https://registry.npmjs.org/${pkg}/latest`);
+    const data = await response.json();
+
+    latestVersion = data.version;
+    return latestVersion !== version;
+  } catch {
+    return false;
+  }
+};
+
+const continueInstall = async () => {
+  const { key: enterKey } = await inquirer.prompt({
+    name: "key",
+    type: "press-to-continue",
+    enter: true,
+    message: "Press Enter to continue...",
+  });
+};
+
+(async () => {
+  if (await checkpkgoutdated("create-aoijs-bot")) {
+    console.log(
+      `A newer version (${chalk.cyan(
+        "v" + latestVersion
+      )}) is available to download, do you wish to continue?`
+    );
+    await continueInstall();
+    await installer.execute("npm install -g create-aoijs-bot@latest");
+    console.log(
+      `Successfully updated to the latest version (${chalk.cyan(
+        "v" + latestVersion
+      )}), now running the version command again. \n\r\n\r`
+    );
+    await installer.execute("create-aoijs-bot --version");
+  }
+})();
